c275ea14d5e0235b3ef74a94f29be7dd86c73a34
Carl Clarke <carlclarke1990@me.com>
2022-07-26 19:36:36 +0200

    feat: Yup (formcontroller) & initalvalues val sch

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import {Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 function CreatePost() {
+
+    const initialValues = {
+        title: "",
+        postText: "",
+        username: ""
+    };
+    const validationSchema = Yup.object().shape({
+        title: Yup.string().required(),
+        postText: Yup.string().required(),
+        username: Yup.string().min(3).max(15).required()
+
+    });
+
+
+    const onSubmit = (data) =>{
+        console.log(data);
+    };
   return (
     <div className="createPostPage">
-        <Formik >
+        <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema ={validationSchema}>
             <Form className='formContainer'>
                 <label>Title:</label>
                 <Field 
